f1101497eebca07cb64c8151ab72eb384c5f6013
José Andrés <antasalmeriajose@gmail.com>
2021-04-11 12:05:31 +0200

    Initial commit

---
+++ app/Http/Controllers/ForoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\foro;
+use Illuminate\Http\Request;
+
+class ForoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\foro  $foro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(foro $foro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\foro  $foro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(foro $foro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\foro  $foro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, foro $foro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\foro  $foro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(foro $foro)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_04_11_095422_create_temas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->foreign('id_foro');
+            $table->foreign('id_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temas');
+    }
+}
