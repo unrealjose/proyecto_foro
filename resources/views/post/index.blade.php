eb313cf1917560f4b4ee0a1f5b74f4d45474aa0f
José Andrés <antasalmeriajose@gmail.com>
2021-05-04 11:59:43 +0200

    Cambios importantes en los Post

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -13,7 +13,7 @@
                         <tr class="text-left border-b-2 border-gray-300">
                           <th class="px-4 py-3">Creador</th>
                           <th class="px-4 py-3">Mensaje</th>
-                          <th class="px-4 py-3">Fecha de creacion</th>
+                          <th class="px-4 py-3">Fecha</th>
                           <th class="px-4 py-3">Rango user (Provisional)</th>
                           <th class="px-4 py-3">Acciones</th>
                         </tr>
@@ -40,10 +40,10 @@
                                             @csrf
                                             @method('DELETE')
                                             @if ($user[$id_user]->id == $user_id)
-                                                <a href="{{route('post.edit',$item)}}"><button class="bg-blue-400">Editar</button></a>
+                                                <a href="{{route('post.edit',$item)}}" class="bg-blue-400 rounded p-1 m-1">Editar</a>
                                                 <button type="submit" class="bg-red-400">Borrar</button>
                                             @elseif ($user[($user_id-1)]->rango == 2)
-                                            <button type="submit" class="bg-yellow-400">Moderar</button>
+                                            <button type="submit" class="bg-yellow-400 rounded p-1 m-1">Moderar</button>
                                             @endif
                                         </form>
                                     </td>
@@ -52,6 +52,34 @@
                             @endforeach
                         </tr>
                     </table>
+                    <!-- aaaa -->
+                    @foreach ($posts as $item)
+                        @php
+                            $num_id_user = intval($item->user_id);
+                            $id_user = $num_id_user-1;
+                        @endphp
+                        <div class="max-w-md w-full">
+                            <div class="bg-gray-300 border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                                <div class="flex items-center">
+                                    <img class="w-10 h-10 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg" alt="Avatar of Jonathan Reinink">
+                                    <div class="text-sm">
+                                        <p class="text-black leading-none">{{$user[$id_user]->name}}</p>
+                                        <p class="text-grey-dark">{{$item->updated_at}}</p>
+                                    </div>
+                                </div>
+                                <div class="mb-8">
+                                    <br>
+                                    @if ($item->moderado == 1)
+                                        <p class="text-grey-darker text-base"><i>{{$item->mensaje}}</i></p>
+                                    @else
+                                        <p class="text-grey-darker text-base">{{$item->mensaje}}</p>
+                                    @endif
+                                </div>
+
+                            </div>
+                        </div>
+                    @endforeach
+                    <!-- aaaa -->
                 </div>
                 <form name="form" method="POST" action="{{route('post.store')}}">
                     @csrf
@@ -64,3 +92,7 @@
         </div>
     </div>
 </x-app-layout>
+
+
+<!-- https://tailwindcomponents.com/component/horizontal-card -->
+<!-- https://tailwindcomponents.com/component/simple-card -->
