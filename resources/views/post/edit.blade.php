9f822dcbd34880ee5d082b68d4c0e0ba64e2593a
José Andrés <antasalmeriajose@gmail.com>
2021-05-04 18:10:41 +0200

    Mejora creacion de temas

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
 
                     <div class="w-full m-1">
-                        <div class="bg-gray-400 border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                        <div class="rounded bg-gray-400 border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                             <div class="flex items-center">
                                 <img class="w-10 h-10 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg" alt="Avatar of Jonathan Reinink">
                                 <div class="text-sm">
@@ -23,7 +23,7 @@
                                     @csrf
                                     @method('PUT')
                                     <br>
-                                    <textarea class="bg-gray-300" name="mensaje" placeholder="{{$post->mensaje}}" required></textarea>
+                                    <textarea class="bg-gray-300 rounded" name="mensaje" placeholder="{{$post->mensaje}}" required></textarea>
                                     <input type="hidden" name="foro_id" value="{{$post->foro_id}}">
                                     <input type="hidden" name="tema_id" value="{{$post->tema_id}}">
                                     <div class="mt-2">
--- resources/views/tema/create.blade.php
+++ resources/views/tema/create.blade.php
@@ -10,24 +10,23 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <form name="form" action="{{route('tema.store')}}" method="POST" enctype="multipart/form-data" class="mt-3">
-                        @csrf
-                        <div class="row">
-                            <div class="col-2">
-                                <input type="text" name="nombre" required placeholder="Nombre Tema" class="form-control">
-                                <input type="hidden" name="foro_id" value="{{$foro_id}}">
+                    <div class="w-full m-1">
+                        <div class="rounded bg-gray-400 border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                            <div class="mb-8">
+                                <form name="form" action="{{route('tema.store')}}" method="POST" class="mt-3">
+                                    @csrf
+                                    <br>
+                                    <input type="text" class="bg-gray-300" name="nombre" required placeholder="Nombre Tema" class="form-control">
+                                    <input type="hidden" name="foro_id" value="{{$foro_id}}">
+                                    <div class="mt-2">
+                                        <button type="submit" class="bg-green-300 p-1 rounded">Crear tema</button>
+                                        <button type="reset" class="bg-red-300 p-1 rounded">Borrar</button>
+                                        <a href="{{route('foro.index')}}" class="bg-yellow-300 p-1 rounded">Volver</a>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="row mt-3">
-                            <div class="col">
-                                <button type="submit" class="btn btn-success">Crear Tema</button>
-                                <button type="reset" class="btn btn-warning">Borrar</button>
-                                <a href="{{route('foro.index')}}" class="btn btn-primary">Volver</a>
-                            </div>
-                        </div>
-                    </form>
-
+                    </div>
                 </div>
             </div>
         </div>
