dc762187bc80925beb33a297670fecd14451a52a
José Andrés <antasalmeriajose@gmail.com>
2021-05-06 17:38:07 +0200

    Rompiendo cosas

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -87,64 +87,54 @@
                     </div>
                 </div>
 
-                <!-- Cosas -->
-
-
-  <!--Modal-->
-  <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
-    <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
-
-
-    <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
-
-      <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
-        <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-          <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-        </svg>
-        <span class="text-sm">(Esc)</span>
-      </div>
-
-      <!-- Add margin if you want to see some of the overlay behind the modal -->
-      <div class="modal-content py-4 text-left px-6">
-        <!-- Title -->
-        <div class="flex justify-between items-center pb-3">
-          <p class="text-2xl font-bold">Editar mensajes</p>
-          <div class="modal-close cursor-pointer z-50">
-            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-            </svg>
-          </div>
-        </div>
-
-        <!-- Body -->
-        <form name="form" action="{{route('post.update', $posts)}}" method="POST" class="mt-3">
-            @csrf
-            @method('POST')
-            <br>
-            <textarea class="bg-gray-300 rounded" name="mensaje" placeholder="#" required></textarea>
-            <input type="hidden" name="foro_id" value="#">
-            <input type="hidden" name="tema_id" value="#">
-            <div class="mt-2">
-                <button type="submit" class="bg-green-300 p-1 rounded">Actualizar Post</button>
-                <button type="reset" class="bg-red-300 p-1 rounded">Borrar</button>
-                <a href="{{route('foro.index')}}" class="bg-yellow-300 p-1 rounded">Volver</a>
-            </div>
-        </form>
-
-        <p>Antiguo mensaje</p>
-        <textarea name="mensaje" id="mensaje" required>#</textarea>
-
-        <!-- Footer -->
-        <div class="flex justify-end pt-2">
-          <button class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
-          <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
-        </div>
-
-      </div>
-    </div>
-  </div>
-                <!-- Cosas -->
-
+                <!-- Cosas Modales-->
+                <!--Modal-->
+                <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+                    <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+                    <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+                        <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                            <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            </svg>
+                            <span class="text-sm">(Esc)</span>
+                        </div>
+                        <!-- Add margin if you want to see some of the overlay behind the modal -->
+                        <div class="modal-content py-4 text-left px-6">
+                            <!-- Title -->
+                            <div class="flex justify-between items-center pb-3">
+                                <p class="text-2xl font-bold">Editar mensajes</p>
+                                <div class="modal-close cursor-pointer z-50">
+                                    <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <!-- Body -->
+                            <form name="form" action="{{route('post.update', $posts)}}" method="POST" class="mt-3">
+                                @csrf
+                                @method('POST')
+                                <br>
+                                <textarea class="bg-gray-300 rounded" name="mensaje" placeholder="#" required></textarea>
+                                <input type="hidden" name="foro_id" value="#">
+                                <input type="hidden" name="tema_id" value="#">
+                                <div class="mt-2">
+                                    <button type="submit" class="bg-green-300 p-1 rounded">Actualizar Post</button>
+                                    <button type="reset" class="bg-red-300 p-1 rounded">Borrar</button>
+                                    <a href="{{route('foro.index')}}" class="bg-yellow-300 p-1 rounded">Volver</a>
+                                </div>
+                            </form>
+                            <p>Antiguo mensaje</p>
+                            <textarea name="mensaje" id="mensaje" required>#</textarea>
+                            <!-- Footer -->
+                            <div class="flex justify-end pt-2">
+                                <button class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
+                                <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Cosas Modales-->
+                
             </div>
             <div class="mt-2">
                 {{ $posts->links() }}
@@ -182,7 +172,6 @@
     	    toggleModal()
         }
     };
-
 
     function toggleModal () {
         const body = document.querySelector('body')
